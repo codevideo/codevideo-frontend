82d9e00dec71193be8bb5204d086f4dcc7cd850f
Chris Frewin <frewin.christopher@gmail.com>
2024-03-19 20:21:09 +0100

    add type declarations!

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,39 @@
-import typescript from '@rollup/plugin-typescript';
-import json from '@rollup/plugin-json';
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/esm/index.js',
-    format: 'module',
-    sourcemap: true,
-    strict: false,
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/esm/index.js",
+      format: "module",
+      sourcemap: true,
+      strict: false,
+    },
+    plugins: [typescript(), json()],
+    external: [
+      "@ffmpeg/ffmpeg",
+      "@ffmpeg/util",
+      "monaco-editor-core",
+      "@fullstackcraftllc/virtual-code-block",
+    ],
   },
-  plugins: [typescript(), json()],
-  external: ['@ffmpeg/ffmpeg', '@ffmpeg/util', 'monaco-editor-core', '@fullstackcraftllc/virtual-code-block']
-};
+  // type declarations
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "es",
+      },
+    ],
+    plugins: [dts()],
+    external: [
+      "@ffmpeg/ffmpeg",
+      "@ffmpeg/util",
+      "monaco-editor-core",
+      "@fullstackcraftllc/virtual-code-block",
+    ],
+  },
+];
